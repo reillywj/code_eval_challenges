7a78f8d00f045182c3957fa23ccc061644f8c35d
Will Reilly <william.reilly2010@gmail.com>
2016-02-21 02:16:18 +0000

    Matrix Rotation completed and passed.

--- matrix_rotation/matrix_rotation.rb
+++ matrix_rotation/matrix_rotation.rb
@@ -1,4 +1,30 @@
+require 'pry'
 class Matrix
   def initialize(letters)
+    @letters = letters.split(' ')
+    @size_matrix = Math.sqrt(@letters.size).to_i
+    @matrix = []
+    (0...@size_matrix).each do |row|
+      @matrix << @letters[row*@size_matrix, @size_matrix]
+    end
+  end
+  
+  def rotate
+    solution = []
+    temp_matrix = @matrix.map.to_a
+    counter = reset_counter
+    (@size_matrix * @size_matrix).times do
+      solution << temp_matrix[counter].shift
+      counter -= 1
+      counter = reset_counter if counter < 0
+    end
+
+    solution.join(' ')
+  end
+  
+  private
+  
+  def reset_counter
+    @size_matrix - 1
   end
 end
